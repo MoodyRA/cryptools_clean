051142e8df8e7685655b246be0700929bd45459f
DESKTOP-UAFCRAM\Ronan <ronan.aupetit@5flow.com>
2022-04-08 22:36:22 +0200

    création d'une entité représentant un compte API Binance

---
+++ src/Domain/Binance/Entity/BinanceApiAccount.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Cryptools\Domain\Binance\Entity;
+
+class BinanceApiAccount
+{
+    /**
+     * @var int
+     */
+    private int $id;
+    /**
+     * @var string
+     */
+    private string $key;
+    /**
+     * @var string
+     */
+    private string $secret;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return BinanceApiAccount
+     */
+    public function setId(int $id): BinanceApiAccount
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     * @return BinanceApiAccount
+     */
+    public function setKey(string $key): BinanceApiAccount
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecret(): string
+    {
+        return $this->secret;
+    }
+
+    /**
+     * @param string $secret
+     * @return BinanceApiAccount
+     */
+    public function setSecret(string $secret): BinanceApiAccount
+    {
+        $this->secret = $secret;
+        return $this;
+    }
+}
