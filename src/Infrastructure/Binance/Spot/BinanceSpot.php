c6025ce092b37665dc0f35dcd8779cc4a1e1fd4e
DESKTOP-UAFCRAM\Ronan <ronan.aupetit@5flow.com>
2022-04-09 01:27:58 +0200

    commit intermediaire - des choses à revoir notamment sur les quotas d'appels + tout ce code doit-il être dans cette classe ?

--- src/Infrastructure/Binance/Spot/BinanceSpot.php
+++ src/Infrastructure/Binance/Spot/BinanceSpot.php
@@ -6,9 +6,13 @@ namespace Cryptools\Infrastructure\Binance\Spot;
 
 use Binance\API as BinanceApi;
 use Cryptools\Domain\Binance\Entity\BinanceApiAccount;
+use Cryptools\Domain\Currency\Collection\PairCollection;
+use Cryptools\Domain\Currency\Entity\Cryptocurrency;
 use Cryptools\Domain\Currency\Entity\Pair;
 use Cryptools\Domain\Wallet\Collection\TradeCollection;
+use Cryptools\Domain\Wallet\Collection\WalletCryptocurrencyCollection;
 use Cryptools\Domain\Wallet\Entity\Trade;
+use Cryptools\Domain\Wallet\Entity\WalletCryptocurrency;
 use Cryptools\Domain\Wallet\Enum\TradeType;
 
 class BinanceSpot implements BinanceSpotInterface
@@ -52,6 +56,7 @@ class BinanceSpot implements BinanceSpotInterface
     public function trades(Pair $pair): TradeCollection
     {
         try {
+            $this->beforeDoingRequest();
             $entries = $this->binanceApi->history(
                 $pair->getBaseCurrency()->getSymbol() . $pair->getQuoteCurrency()->getSymbol()
             );
@@ -73,8 +78,98 @@ class BinanceSpot implements BinanceSpotInterface
         }
     }
 
+    /**
+     * @return TradeCollection
+     * @throws \Exception
+     */
+    public function tradesFromOwnedCurrencies(): TradeCollection
+    {
+        $cryptocurrencies = $this->ownedCryptocurrencies();
+        $pairs = $this->allPairs();
+        $pairsByBaseCurrency = $pairs->toArrayByBaseCurrencySymbol();
+        $trades = new TradeCollection([]);
+        /** @var WalletCryptocurrency $cryptocurrency */
+        foreach ($cryptocurrencies as $cryptocurrency) {
+            $cryptocurrencyPairs = $pairsByBaseCurrency[$cryptocurrency->getSymbol()];
+            foreach ($cryptocurrencyPairs as $pair) {
+                $trades = $trades->mergeWith($this->trades($pair));
+            }
+        }
+        return $trades;
+    }
+
+    /**
+     * @return TradeCollection
+     * @throws \Exception
+     */
     public function allTrades(): TradeCollection
     {
-        // TODO: Implement allTrades() method.
+        $pairs = $this->allPairs();
+        $trades = new TradeCollection([]);
+        /** @var Pair $pair */
+        foreach ($pairs->getIterator() as $pair) {
+            $trades = $trades->mergeWith($this->trades($pair));
+        }
+        return $trades;
+    }
+
+    /**
+     * @return WalletCryptocurrencyCollection
+     */
+    public function ownedCryptocurrencies(): WalletCryptocurrencyCollection
+    {
+        try {
+            $this->beforeDoingRequest();
+            $prices = $this->binanceApi->prices();
+            $this->beforeDoingRequest();
+            $balances = $this->binanceApi->balances($prices);
+
+            $cryptocurrencies = [];
+            foreach ($balances as $symbol => $asset) {
+                if (isset($asset['available']) && (float)$asset['available'] > 0) {
+                    $quantity = (float)$asset['available'] + (float)$asset['onOrder'];
+                    $cryptocurrency = new WalletCryptocurrency();
+                    $cryptocurrency->setSymbol($symbol)
+                        ->setQuantity($quantity);
+                    $cryptocurrencies[] = $cryptocurrency;
+                }
+            }
+            return new WalletCryptocurrencyCollection($cryptocurrencies);
+        } catch (\Exception $e) {
+        }
+    }
+
+    /**
+     * @return PairCollection
+     */
+    public function allPairs(): PairCollection
+    {
+        try {
+            $this->beforeDoingRequest();
+            $infos = $this->binanceApi->exchangeInfo();
+            $pairs = [];
+            foreach ($infos['symbols'] as $symbol) {
+                $pair = new Pair();
+                $cryptoBase = (new Cryptocurrency())->setSymbol($symbol['baseAsset']);
+                $cryptoQuote = (new Cryptocurrency())->setSymbol($symbol['quoteAsset']);
+                $pair->setBaseCurrency($cryptoBase)
+                    ->setQuoteCurrency($cryptoQuote);
+                $pairs[] = $pair;
+            }
+            return new PairCollection($pairs);
+        } catch (\Exception $e) {
+        }
+    }
+
+    /**
+     * L'api binance n'accepte pas plus de 1200 requêtes par minute, on patiente donc si on arrive à la limite
+     * @return void
+     */
+    private function beforeDoingRequest()
+    {
+        $transfered = $this->binanceApi->getTransfered();
+        if (($this->binanceApi->getRequestCount() % 1150) === 0) {
+            sleep(60);
+        }
     }
 }
